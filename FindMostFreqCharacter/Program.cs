bf06f781d5e7d9ee698282a78672af4c1a4dda6e
oguiulfo <orguiulfo@gmail.com>
2014-09-05 00:24:54 -0700

    Initial check-in.

---
+++ FindMostFreqCharacter/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindMostFreqCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string, any string, and hit Enter!");
+            FindMostFreqCharacter(Console.ReadLine());
+
+            while (1 == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Would you like to play again?");
+                Console.WriteLine("Enter another string or type 'exit' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "exit")
+                    Environment.Exit(0);
+                else
+                    FindMostFreqCharacter(input);
+            }
+        }
+
+        static void FindMostFreqCharacter(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                Console.WriteLine("This string is null or empty.");
+                return;
+            }
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var listCharacters = new List<KeyValuePair<char, int>>(); // Key = character, Value = frequency count
+            foreach (char c in str)
+            {
+                var ch = listCharacters.FirstOrDefault(x => x.Key == c);
+                if (ch.Key == c)
+                {
+                    listCharacters.Remove(ch);
+                    listCharacters.Add(new KeyValuePair<char, int>(ch.Key, ch.Value + 1));
+                }
+                else
+                {
+                    listCharacters.Add(new KeyValuePair<char, int>(c, 1));
+                }
+            }
+
+            if (listCharacters.Any())
+            {
+                int maxVal = listCharacters.Max(v => v.Value);
+                var chars = listCharacters.Where(k => k.Value.Equals(maxVal));
+
+                if (chars.Count() == 1)
+                {
+                    foreach (var character in chars)
+                        Console.WriteLine("The character '{0}' appears the most frequently in this string.", (char)character.Key);
+                }
+                else if (chars.Count() > 1)
+                {
+                    var keys = chars.Select(x => x.Key).ToList();
+                    Console.WriteLine("The characters '{0}' all appear with the same amount of frequency.", string.Join(", ", keys));
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("<<This operation took {0} ticks to execute>>", stopWatch.ElapsedTicks);
+        }
+    }
+}
